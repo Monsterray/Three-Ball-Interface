12ad6964c3c87272cfdb9f30089e056f6b5ba3ea
Monsterray <inkrpen@gmail.com>
2017-02-26 03:03:52 -0800

    Changed most of the code to get it to run without errors Added some informational prints Made the program make the file if the it can't find the file Made the try/catch blocks smaller to encapsulate just what they are for

--- Three Ball Interface/src/ThreeBall.java
+++ Three Ball Interface/src/ThreeBall.java
@@ -15,34 +15,47 @@ public class ThreeBall{
 		System.out.println("Three Ball scorer loaded");
 	}
 
-	public void loadPlayers(){ //doesnt work started never finished
-		try{
-			File f = new File("./Highscores.TSM");
-			Scanner s = new Scanner(f);
-			while (s.hasNextLine()) {
-				String line = s.nextLine();
-				if (line.startsWith("#")) //if it finds a line that begins with a # it will skip that line, its like a comment
-					continue;
-				StringTokenizer normalTok = new StringTokenizer(line, "\t"); //every tab it will take in the next variable
-				playersScores = new String[normalTok.countTokens()][2];
-				playersScores[0][0] = normalTok.nextToken();
-				int count = 1; //creates the int count
-				while (normalTok.hasMoreTokens()) { //separates the rest of the pairs
-					playersScores[count][0] = normalTok.nextToken(); //parses first one and puts it into the huge array
-					count++; //its the i for this while loop
-				}
-			}
-			s.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("debug: problem at loading clue Rewards");
-		}
+	public void loadPlayers(){ // Doesn't work started never finished
+		File f = new File("./Highscores.TSM");
 		
+		Scanner s = null;
+		try {
+			s = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			System.out.println("[ERROR] Couldn't find Highscores.TSM file!");
+			System.out.println("[INFO] Trying to create Highscores.TSM!");
+			try {
+				f.createNewFile();
+			} catch (IOException e1) {
+				System.out.println("[ERROR] Couldn't Create Highscores.TSM file!");
+			}
+			System.out.println("[INFO] Created Highscores.TSM!");
+		}
+		while (s.hasNextLine()) {
+			String line = s.nextLine();
+			if (line.startsWith("#")) //if it finds a line that begins with a # it will skip that line, its like a comment
+				continue;
+			
+			StringTokenizer normalTok = new StringTokenizer(line, "\t"); //every tab it will take in the next variable
+			playersScores = new String[normalTok.countTokens()][2];
+			playersScores[0][0] = normalTok.nextToken();
+			
+			int count = 1; //creates the int count
+			while (normalTok.hasMoreTokens()) { //separates the rest of the pairs
+				playersScores[count][0] = normalTok.nextToken(); //parses first one and puts it into the huge array
+				count++; //its the i for this while loop
+			}
+		}
+		s.close();
 	}
 	
 	public static void arrayPrint(String[] args){
 		for(int i = 0; i <= args.length - 1; i++){
 			System.out.println(args[i]);
 		}
+	}
+	
+	public static void main(String[] args){
+		new ThreeBall();
 	}
 }
