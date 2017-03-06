9bd65bfb3774f7f18a87e6f2f134b9893fd6a8f4
Monsterray <inkrpen@gmail.com>
2017-03-06 01:47:19 -0800

    1. Got the file loader to work correctly 2. Made it so if the score file didn't exsist it would create a default file at the location it looks for 3. Added countLines(File) to count the amount of lines in a file 4. Added createNewScoreFile(File) to encapsulate the creation of a new score file Made debug messages more dynamic

--- Three Ball Interface/src/ThreeBall.java
+++ Three Ball Interface/src/ThreeBall.java
@@ -7,51 +7,95 @@ import java.util.Scanner;
 
 public class ThreeBall{
 
+	// Need to change this to a HashMap<String><ArrayList<Integer>>
 	public String[][] playersScores;
+	public static String scoreFileLocation = "./Highscores.TSM";
 
 	public ThreeBall(){
 		loadPlayers();
 		
 		System.out.println("Three Ball scorer loaded");
+		
+//		arrayPrint(playersScores);
 	}
 
-	public void loadPlayers(){ // Doesn't work started never finished
-		File f = new File("./Highscores.TSM");
+	public void loadPlayers(){
+		File f = new File(scoreFileLocation);
+		int lines = countLines(f);
 		
 		Scanner s = null;
 		try {
 			s = new Scanner(f);
 		} catch (FileNotFoundException e) {
-			System.out.println("[ERROR] Couldn't find Highscores.TSM file!");
-			System.out.println("[INFO] Trying to create Highscores.TSM!");
-			try {
-				f.createNewFile();
-			} catch (IOException e1) {
-				System.out.println("[ERROR] Couldn't Create Highscores.TSM file!");
-			}
-			System.out.println("[INFO] Created Highscores.TSM!");
+			System.out.println("[ERROR] Couldn't find " + f.getName() + "!");
+			createNewScoreFile(f);
 		}
+		
+		int i = 0;
+		playersScores = new String[lines][4]; // playersScores[Name][Best Score][Wins][Loses]
+		
 		while (s.hasNextLine()) {
 			String line = s.nextLine();
 			if (line.startsWith("#")) // If it finds a line that begins with a # it will skip that line, its like a comment
 				continue;
-			
+			// Need to make it so that if they put too many tabs/whitespace in it will be of
 			StringTokenizer normalTok = new StringTokenizer(line, "\t"); // Every tab it will take in the next variable
-			playersScores = new String[normalTok.countTokens()][2];
-			
-			int count = 0; // Creates the int count
-			while (normalTok.hasMoreTokens()) { // Separates the rest of the pairs
-				playersScores[count][0] = normalTok.nextToken(); // Parses first one and puts it into the huge array
-				count++; // Its the i for this while loop
+		
+			for (int j = 0; normalTok.hasMoreTokens(); j++) { // Adds the data on each line to the 3D array
+				playersScores[i][j] = normalTok.nextToken(); // Parses next token and puts it into the array
 			}
+			i++;
 		}
 		s.close();
 	}
 	
-	public static void arrayPrint(String[] args){
+	public void arrayPrint(String[][] args){
 		for(int i = 0; i <= args.length - 1; i++){
-			System.out.println(args[i]);
+			for(int j = 0; j <= args[i].length - 1; j++){
+				System.out.print(args[i][j] + "\t");
+			}
+			System.out.println();
 		}
+	}
+	
+	public int countLines(File file){
+		File f = new File("./Highscores.TSM");
+		Scanner s = null;
+		int amountLines = 0;
+		
+		try {
+			s = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			System.out.println("[ERROR] Couldn't find " + file.getName() + "!");
+		}
+		while (s.hasNextLine()) {
+			if (s.nextLine().startsWith("#")) // If it finds a line that begins with a # it will skip that line, its like a comment
+				continue;
+			amountLines++;
+		}
+		s.close();
+		
+		return amountLines;
+	}
+	
+	public boolean createNewScoreFile(File file){
+		System.out.println("[INFO] Trying to create " + file.getName() + "!");
+		Writer writer;		// This writes data to the file
+		try {
+			file.createNewFile();
+			FileOutputStream is = new FileOutputStream(file);
+			OutputStreamWriter osw = new OutputStreamWriter(is);
+			writer = new BufferedWriter(osw);
+			
+			writer.write("#Name\tBest\tScore\tWins\tLoses");
+			writer.close();
+			
+			System.out.println("[INFO] Created " + file.getName() + "!");
+			return true;
+		} catch (IOException ioe) {
+			System.out.println("[ERROR] Couldn't Create " + file.getName() + "!");
+			return false;
+		}		
 	}
 	
 	public static void main(String[] args){
